a74e1c27f7b5034a8981a042b17ec5e09bc33426
datboi_fourtwenty <rescus1221@gmail.com>
2020-04-23 01:07:04 -0400

    Added some logging Made change to status routes status/twitch_id gets a sepecific status /status gets all bots from the DB

--- twitchBot.js
+++ twitchBot.js
@@ -1,42 +1,61 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
+const reconnect = require('./app');
 
 const bot = async () => {
   client = new tmi.client({
     options: {
-      debug: false
+      clientId: process.env.CLIENT_ID,
+      debug: false,
+    },
+    connection: {
+      reconnect: true,
+      // secure: true,
     },
     identity: {
       username: process.env.BOT_USERNAME,
-      password: process.env.OAUTH_TOKEN
-    }
+      password: process.env.OAUTH_TOKEN,
+    },
   });
 
   client.on('message', onMessageHandler);
+  client.on('connected', (addr, port) => {
+    console.log(`Client connected at ${addr}:${port}`);
+  });
+  client.on('disconnected', (reason) => {
+    console.log('Connection closed: ', { reason });
+  });
+  client.on('join', (channel, username, self) => {
+    if (self) {
+      console.log(`Joined ${channel}`);
+    }
+  });
+  client.on('reconnect', reconnect(true));
+  client.on('part', (channel, username, self) => {
+    if (self) {
+      console.log(`Parted ${channel}`);
+    }
+  });
   await client.connect();
   return client;
 };
 
 function onMessageHandler(from, context, msg, self) {
-  if (self) { return; }
+  if (self) {
+    return;
+  }
 
   const commandName = msg.trim();
   if (commandName === '!buffs') {
-    // client.whisper(context.username, `Here is your referral link: buffs.app/r/${from.slice(1)}/${context.username}`)
-    //   .then((data) => {
-    //     // console.log(data);
-    //   })
-    //   .catch((err) => {
-    //     // console.error(err);
-    //   });
-    client.say(from, `@${context.username} Here is your referral link: buffs.app/r/${from.slice(1)}/${context.username}`)
-      .then((data) => {
-        // console.log(data);
-      })
-      .catch((err) => {
-        // console.error(err);
-      });
+    client.say(
+      from,
+      `@${
+        context.username
+      } Here is your referral link: buffs.app/r/${from.slice(1)}/${
+        context.username
+      }`
+    );
   }
-};
+}
 
 module.exports = bot;
