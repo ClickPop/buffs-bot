fee82c59276e6dd99074db008e60503f15e3bde6
datboi_fourtwenty <rescus1221@gmail.com>
2020-04-24 18:08:24 -0400

    Working on auth for create route Pushing to work on laptop

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Bot = require('../db/models/schema');
 const Hashids = require('hashids/cjs');
+const axios = require('axios');
 const hashids = new Hashids(process.env.SALT || ENV['SALT'], 32);
 
 const authenticate = async (req, res, next) => {
@@ -13,19 +14,34 @@ const authenticate = async (req, res, next) => {
   try {
     const auth = hashids.decode(req.headers.authorization);
 
-    const user = await Bot.findOne({ twitch_userId: auth });
+    if (!req.path.includes('create')) {
+      const user = await Bot.findOne({ twitch_userId: auth });
 
-    if (!user) {
-      return res.status(422).json({
-        errors: { error: 'Missing or invalid authorization header' },
+      if (!user) {
+        return res.status(422).json({
+          errors: { error: 'Missing or invalid authorization header' },
+        });
+      }
+
+      req.userInfo = {
+        twitch_username: user.twitch_username,
+        twitch_userId: user.twitch_userId,
+      };
+    } else {
+      const twitch = await axios.get('https://api.twitch.tv/helix/users', {
+        headers: {
+          authorization: `Bearer ${
+            process.env.OAUTH_TOKEN || ENV['OAUTH_TOKEN']
+          }`,
+        },
+        params: {
+          login: req.body.twitch_username,
+        },
       });
+
+      console.log(twitch.data.data[0].id === auth);
+      return res.json({ data: 'test' });
     }
-
-    req.userInfo = {
-      twitch_username: user.twitch_username,
-      twitch_userId: user.twitch_userId,
-    };
-
     next();
   } catch (err) {
     console.error(err);
