e84761f3b6d4037388709409985672202d79dc70
datboi_fourtwenty <rescus1221@gmail.com>
2020-04-24 08:37:15 -0400

    Added ENV info for Heroku

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ app.delete(
   }
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || ENV['PORT'] || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
