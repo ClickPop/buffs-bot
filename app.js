5793bdf1911139fada3af74f967b401c90517697
datboi_fourtwenty <rescus1221@gmail.com>
2020-04-25 04:52:17 -0400

    Fixed welcome route authentication

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const authenticate = require('./middleware/authenticate');
 const isAdmin = require('./middleware/isAdmin');
 const reconnect = require('./util/reconnect');
 const axios = require('axios');
-
 app = express();
 app.use(bodyParser.json());
-app.use(authenticate);
 
 //Connect to mongoDB
 connectDB();
@@ -19,28 +17,26 @@ connectDB();
 //Initialize and reconnect bots if server restarted
 reconnect();
 
-//Call the heroku app every 10 minutes to keep it from sleeping
-setInterval(() => {
-  axios.get('http://buffsbot.herokuapp.com/');
-}, 1000 * 60 * 10);
-
-app.use('/', (req, res) => {
-  res.json({
+// //Home Route
+app.get('/', (req, res) => {
+  return res.json({
     data: {
       message: 'WELCOME TO THE BUFFS BOT API',
     },
   });
 });
+//Authentication Middleware
+app.use(authenticate);
+//Admin routes
 app.use('/api/admin', isAdmin, require('./routes/admin'));
+//Standard routes
 app.use('/api', require('./routes/standard'));
 
+//Error handling
 app.use((res, req, next) => {
   next(createError(404));
 });
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  // render the error page
   res.status(err.status || 500);
   if (err.status === 404) {
     res.json(err);
@@ -54,3 +50,8 @@ const PORT = process.env.PORT || ENV['PORT'] || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+//Call the heroku app every 10 minutes to keep it from sleeping
+setInterval(async () => {
+  await axios.get('http://buffsbot.herokuapp.com/');
+}, 1000 * 60 * 10);
