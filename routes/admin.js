ee6748dac5d00b15e9366541c8efd043d54cbc61
datboi_fourtwenty <rescus1221@gmail.com>
2020-05-19 03:15:49 -0400

    Adjusted the admin route to be able to take a list of userIds to get data on

--- routes/admin.js
+++ routes/admin.js
@@ -232,29 +232,43 @@ router.delete(
 );
 
 router.get('/views', async (req, res) => {
-  const { twitch_userId } = req.query;
-  let bot = await Bot.findOne({ twitch_userId });
-  if (!bot) {
+  const { twitch_userIds } = req.query;
+  let stream_count = 0;
+  let totalViews = [];
+  let bots = await Bot.find({
+    twitch_userId: {
+      $in: twitch_userIds.split(',').map((item) => {
+        return item.trim();
+      }),
+    },
+  });
+  if (!bots) {
     return res.status(404).json({ errors: 'No bot found' });
   }
   const { from, to } = req.query;
-  let streams = await Stream.find({
-    bot: bot.id,
-    started_at: {
-      $gte: moment(from).utc().format(),
-      $lte: moment(to).utc().format(),
-    },
-  });
-  if (!streams) return res.status(404).json({ errors: 'No streams found' });
-  totalViews = [];
-  await asyncForEach(streams, async (stream) => {
-    let views = await View.find({ stream: stream.id });
-    totalViews.push({
-      stream,
-      views,
+  await asyncForEach(bots, async (bot) => {
+    const query = {
+      bot: bot.id,
+    };
+    if (from || to) {
+      query.started_at = {};
+      if (from) query.started_at.$gte = moment(from).utc().format();
+      if (to) query.started_at.$lte = moment(to).utc().format();
+    }
+    streams = await Stream.find(query);
+    if (!streams) return res.status(404).json({ errors: 'No streams found' });
+    stream_count += streams.length;
+    console.log(streams);
+    await asyncForEach(streams, async (stream) => {
+      let views = await View.find({ stream: stream.id });
+      totalViews.push({
+        stream,
+        view_count: views.length,
+        views,
+      });
     });
   });
-  return res.json(totalViews);
+  return res.json({ stream_count, streams: totalViews });
 });
 
 async function asyncForEach(array, callback) {
