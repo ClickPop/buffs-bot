ffaf5f1cdda905c958f8d29d81e737720b6dc159
datboi_fourtwenty <rescus1221@gmail.com>
2020-05-23 03:34:29 -0400

    Added another check for when the bot parts or joins to setup viewer joined_at/parted_at

--- test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-(async () => {
-  let res = await axios.get('https://api.twitchinsights.net/v1/bots/online');
-  let bots = res.data.bots;
-  bots.forEach((bot) => {
-    console.log(`'${bot[0]}', `);
-  });
-})();
--- twitch/eventHandlers.js
+++ twitch/eventHandlers.js
@@ -6,6 +6,8 @@ const {
   subscribeToWebhook,
   unsubscribeFromWebhook,
 } = require('./webhookSubscriptions');
+const asyncForEach = require('../util/asyncForEach');
+const axios = require('axios');
 
 const joinHandler = async (
   self,
@@ -21,6 +23,40 @@ const joinHandler = async (
       if (self) {
         console.log(`Joined ${channel}`);
         await subscribeToWebhook(twitch_userId, access_token);
+        if (isStreaming) {
+          let stream = await Stream.findOne({ bot: id }).sort({
+            started_at: -1,
+          });
+          if (!stream) return;
+          const views = View.find({
+            stream: stream.id,
+            parted_at: { $exists: false },
+          });
+          const response = await axios.get(
+            `https://tmi.twitch.tv/group/user/${channel.slice(1)}/chatters`
+          );
+          const chatters = [];
+          const viewers = response.data.chatters;
+          for (const viewer_type in viewers) {
+            if (viewer_type === 'broadcaster') break;
+            viewers[viewer_type].forEach((viewer) => {
+              if (!knownBots.includes(viewer)) {
+                chatters.push(viewer);
+              }
+            });
+          }
+          const views_names = views.map((view) => view.twitch_username);
+          asyncForEach(chatters, async (chatter) => {
+            if (!views_names.includes(chatter)) {
+              const view = new View({
+                twitch_username: viewer,
+                stream: stream.id,
+                joined_at: moment().utc(),
+              });
+              await view.save();
+            }
+          });
+        }
         return;
       }
       if (!isStreaming) return;
@@ -61,6 +97,17 @@ const partHandler = (
       if (self) {
         console.log(`Parted ${channel}`);
         await unsubscribeFromWebhook(twitch_userId, access_token);
+        let stream = await Stream.findOne({ bot: id }).sort({ started_at: -1 });
+        if (!stream) return;
+        let views = await View.find({
+          stream: stream.id,
+          parted_at: { $exists: false },
+        });
+        if (!views) return;
+        await asyncForEach(views, async (view) => {
+          view.parted_at = stream.ended_at ? stream.ended_at : moment().utc();
+          await view.save();
+        });
         return;
       }
       if (knownBots.includes(username)) return;
